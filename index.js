bd6dcbd811bc0640342876bdddf1c9f9a143ceb8
cbyungmal <jgaines554@noscihigh.org>
2021-03-17 13:01:50 +0000

    Everything

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 //Declare and initalize a variable with an empty array 
-
+var arr = [];
 
 //Use bracket notation to add
   //a string to the first index
   //a boolean to the second index
   //a number to the fourth index
+[10, 20, 30, 40, 50]
+["spam", "bungee", "swallow"]
+(2, 4, 6, 8)
+("two", "four", "six", "eight")
+[("cheese", "queso"), ("red", "rojo"), ("school", "escuela")]
 
 
 //When you console.log the third and fourth indices 
 //what is the value?
-
+let arr = new Array(element0, element1, ..., elementN)
+let arr = Array(element0, element1, ..., elementN)
+let arr = [element0, element1, ..., elementN]
 
 //What is the length of the array?
-
+length is a property of arrays in JavaScript that returns or sets the number of elements in a given array. The length property of an array can be returned like so. The assignment operator, in conjunction with the length property, can be used to set the number of elements in an array like so.
 
 //Use the push method to insert a string of your favorite tv show.
+const tv shows = ['blacklist', 'lucifer', 'flash'];
+
+const count = tv shows.push('arrow');
+console.log(count);
+// expected output: 4
+console.log(tv shows);
+// expected output: Array ["blacklist", "lucifer", "flash", "arrow"]
+
+animals.push('prodigal son', 'inhumans', 'runaways');
+console.log(tv shows);
+// expected output: Array ["blacklist", "lucifer", "flash", "arrow", "prodigal son", "inhumans", "runaways"]
 
 
 //Use the unshift method to add three values
+const array1 = [1, 2, 3];
+
+console.log(array1.unshift(4, 5));
+// expected output: 5
+
+console.log(array1);
+// expected output: Array [4, 5, 1, 2, 3]
 
 
 //console.log the array
@@ -42,7 +67,11 @@ nextNum(100) => 101
 
 function nextNum(number){
   //CODE BELOW HERE
-
+int invert(int x) 
+{ 
+   if (x == 1) return 2; 
+   else return 1; 
+} 
 
 
   //CODE ABOVE HERE
@@ -64,7 +93,14 @@ EXAMPLE:
 
 function countVowels(string){
   //CODE BELOW HERE
-
+int countVowels(string str) 
+{ 
+    int count = 0; 
+    for (int i=0; i<str.length(); i++) 
+        if (isVowel(str[i])) // Check for vowel 
+            ++count; 
+    return count; 
+} 
 
   //CODE ABOVE HERE
 }
@@ -84,7 +120,12 @@ evenIndicies(otherArray) => false
 
 function evenIndicies(array){
   //CODE BELOW HERE
+int[] res = {0,0};
+    int len = array.length;
 
+    if(array == null){
+        return res;
+    }
 
   //CODE ABOVE HERE
 }
